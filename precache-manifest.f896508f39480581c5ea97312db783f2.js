7023db1b255a1db7d3e3ade93797a83fa930cea5
Alex <39873704+Abbaskhurram255@users.noreply.github.com>
2020-11-03 04:37:00 +0500

    Updates

---
+++ precache-manifest.f896508f39480581c5ea97312db783f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "56d8103d966fb5218f22",
+    "url": "/motivational-quote-generator/static/css/main.ac027222.chunk.css"
+  },
+  {
+    "revision": "56d8103d966fb5218f22",
+    "url": "/motivational-quote-generator/static/js/main.274a77d7.chunk.js"
+  },
+  {
+    "revision": "fc518163369eb1ccd761",
+    "url": "/motivational-quote-generator/static/js/runtime~main.e84954c1.js"
+  },
+  {
+    "revision": "28573762e38f74406f9e",
+    "url": "/motivational-quote-generator/static/js/2.08bc1c37.chunk.js"
+  },
+  {
+    "revision": "11a66229152a7c5fbfbabe073a3b04d9",
+    "url": "/motivational-quote-generator/index.html"
+  }
+];
